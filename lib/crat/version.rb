3245a095b7a87fec166d08cc6cbbf30b31ca3723
William Bolton <william.ellet@gmail.com>
2021-02-12 10:51:03 -0500

    Set Version with Env variables

--- lib/crat/version.rb
+++ lib/crat/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'dotenv/load'
+
 module CRat
-  VERSION = '0.1.0'
+  VERSION = ENV['VERSION']
 end
--- spec/crat_spec.rb
+++ spec/crat_spec.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe CRat do
-  it 'has a version number' do
-    expect(CRat::VERSION).not_to be nil
+  describe 'CRat::VERSION' do
+    it 'has a version number' do
+      expect(CRat::VERSION).not_to be nil
+    end
+    it 'has the right version number set in env' do
+      expect(CRat::VERSION).to eq('0.1.0')
+    end
   end
 
   xit 'does something useful' do
